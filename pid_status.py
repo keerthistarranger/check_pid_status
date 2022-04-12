c1ac5cf7a8842af8c5e45edcbb90aadc83b88cb7
KeerthiBR <keerthibr1@gmail.com>
2022-04-12 17:18:47 -0500

    Add files via upload

---
+++ pid_status.py
@@ -0,0 +1,47 @@
+import requests
+from datetime import datetime
+import getopt, sys
+import urllib3
+import boto3
+import json
+
+
+# Read in command-line parameters
+idle = True
+port = '8443'
+pid = 12345
+ignore_connections = False
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "ht:p:c", ["help","time=","port=","ignore-connections"])
+    if len(opts) == 0:
+        raise getopt.GetoptError("No input parameters!")
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(helpInfo)
+            exit(0)
+        if opt in ("-t", "--time"):
+            time = int(arg)
+        if opt in ("-p")
+            pid = int(arg)
+        if opt in ("-c", "--ignore-connections"):
+            ignore_connections = True
+except getopt.GetoptError:
+    print(usageInfo)
+    exit(1)
+    
+def get_notebook_name():
+    log_path = '/opt/ml/metadata/resource-metadata.json'
+    with open(log_path, 'r') as logs:
+        _logs = json.load(logs)
+    return _logs['ResourceName']
+
+
+import psutil
+if psutil.pid_exists(pid):
+    print("a process with pid %d exists" % pid)
+else:
+    print("a process with pid %d does not exist" % pid)
+    client = boto3.client('sagemaker')
+    client.stop_notebook_instance(
+        NotebookInstanceName=get_notebook_name()
+    )
