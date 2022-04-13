83d7fd2f11d628ac9790769cdf7f8f66ba58a82f
KeerthiBR <keerthibr1@gmail.com>
2022-04-12 20:34:49 -0500

    Update pid_status.py

--- pid_status.py
+++ pid_status.py
@@ -22,7 +22,7 @@ try:
         if opt in ("-t", "--time"):
             time = int(arg)
         if opt in ("-p")
-            pid = int(arg)
+            pid = arg
         if opt in ("-c", "--ignore-connections"):
             ignore_connections = True
 except getopt.GetoptError:
